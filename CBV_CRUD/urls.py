63c8ebf7a13ba8b212eaeeb6477814366d447ef7
bicsu <bicsu@naver.com>
2018-11-24 08:07:32 +0000

    main 홈페이지 메인 추가했음

--- CBV_CRUD/urls.py
+++ CBV_CRUD/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePage.as_view(), name = 'home'),
     path('uni_infor/',include('uni_infor.urls')),
     path('questions/', include('questions.urls')),
     path('accounts/', include('accounts.urls')),
