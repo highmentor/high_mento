71a97f4fae71b4373be5a6427e5e096c3614e393
bicsu <bicsu@naver.com>
2018-11-26 07:08:58 +0000

    김관수16:09

--- majors/models.py
+++ majors/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Major(models.Model):
-    major_id = models.TextField()
-    major_intro = models.TextField()
+    major_id = models.TextField(max_length=100)
+    major_intro = models.TextField(max_length=100)
     
 class Major_review(models.Model):
     major = models.ForeignKey(Major,on_delete=models.CASCADE)
--- majors/urls.py
+++ majors/urls.py
@@ -6,4 +6,7 @@ app_name = "majors"
 
 urlpatterns = [
     path('', views.MajorList.as_view(), name = 'list'),
+    path('create/', views.MajorCreate.as_view(), name='create'),
+    path('<int:pk>/',views.MajorDetailView.as_view(),name='detail'),
+    
 ]
