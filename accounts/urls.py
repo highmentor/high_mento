f43f80c0617772af160b704fc552c0a66b1ee536
bicsu <bicsu@naver.com>
2018-11-27 08:04:36 +0000

    로그인 로그아웃하고 다른 곳으로 이동

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls import url
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('',views.ListView.as_view(), name='list'),
-    path('signup/',views.SignUp.as_view(),name='signup'),
+    path('signup/', views.signup, name='signup'),
+    # path('signup/',views.SignUp.as_view(),name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import CreateView, ListView
 from . import forms
 from django.urls import reverse_lazy
 from .models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import LoginForm, SignupForm
 # Create your views here.
 
-class SignUp(CreateView):
-    form_class = forms.UserCreateForm
-    template_name = 'accounts/signup.html'
-    success_url = reverse_lazy('accounts:login')
-    
 class AccountsList(ListView):
     model = User
     template_name = 'accounts/accounts_list.html'
+    
+def signup(request):
+    if request.method == 'POST':
+        signup_form = SignupForm(request.POST)
+        # 유효성 검증에 통과한 경우 (username의 중복과 password1, 2의 일치 여부)
+        if signup_form.is_valid():
+            # SignupForm의 인스턴스 메서드인 signup() 실행, 유저 생성
+            signup_form.signup()
+            return redirect('/')
+    else:
+        signup_form = SignupForm()
+
+    context = {
+        'signup_form': signup_form,
+    }
+    return render(request, 'accounts/signup.html', context)
